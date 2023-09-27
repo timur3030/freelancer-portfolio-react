cb5363b79ad3f937e9466be7200ed1fc2c74d038
Timur <timur87@mail.ru>
2023-09-27 04:24:52 +0300

    project list

--- src/components/project/Project.js
+++ src/components/project/Project.js
@@ -3,7 +3,7 @@ import React from "react";
 function Project(props) {
   return (
     <li className="projects__item">
-      <a href={props.href}>
+      <a href={props.href} target="_blank" rel="noreferrer">
         <img src={props.img} alt={props.title} className="projects__img" />
         <h3 className="projects__item-title">{props.title}</h3>
       </a>
